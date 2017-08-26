45cbf455923101295233b7e4aee628c5d51bbfb1
Tunde <tundaey@gmail.com>
2017-08-26 14:24:02 +0100

    created and configure babel file

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,22 @@
-module.exports = {
-    entry: ['./app/index.js']
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+odule.exports = {
+    entry: ['./app/index.js'],
+    output: {
+        path : __dirname + '/dist',
+        filename: 'index_bundle.js'
+    },
+    modules: {
+        loaders: [
+            {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+            {test: /\.css$/, loader: 'style-loader!css-loader'}
+        ]
+    },
+    plugins: [HTMLWebpackPluginConfig]
+
 }

bbb943ea3d83101ad1111e2b20c701bf787cfd78
Tunde <tundaey@gmail.com>
2017-09-14 14:18:58 +0100

    added README file with install instructions

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,15 +1,7 @@
 import firebase from 'firebase'
+import {config} from './firebase'
 
 
-// Initialize Firebase
-const config = {
-apiKey: "AIzaSyDBdTbjLenBanM4fwVj5027jGvGxfBHI9k",
-authDomain: "joura-dd7a7.firebaseapp.com",
-databaseURL: "https://joura-dd7a7.firebaseio.com",
-projectId: "joura-dd7a7",
-storageBucket: "joura-dd7a7.appspot.com",
-messagingSenderId: "173305411437"
-};
 
 firebase.initializeApp(config);
 
